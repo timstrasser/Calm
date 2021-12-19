3509cfd25c0c699aff7393c1666520094d6d3e46
Tim Strasser <tim@strasser.lu>
2021-12-19 17:11:19 +0100

    added motiondata

--- Calm/Controllers/MeditationViewController.swift
+++ Calm/Controllers/MeditationViewController.swift
@@ -6,16 +6,45 @@
 //
 
 import UIKit
+import CoreMotion
 
-class MeditationViewController: UIViewController {
-
+class MeditationViewController: UIViewController, CMHeadphoneMotionManagerDelegate {
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    let motionMgr = CMHeadphoneMotionManager();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        guard motionMgr.isDeviceMotionAvailable else {
+            print("Sorry", "Your device is not supported.")
+            return
+        }
+        
+        motionMgr.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: {[weak self] motion, error  in
+            guard let motion = motion, error == nil else { return }
+            self?.motionUpdated(motion)
+        })
+        
+    }
+    
+    func motionUpdated(_ motion: CMDeviceMotion) {
+        let data = motion.rotationRate
+        
+        print(data)
+        
+//        let pitch = String(format: "%.2f", data.pitch)
+//        let roll = String(format: "%.2f", data.roll)
+//        let yaw = String(format: "%.2f", data.yaw)
+        let sum = data.x + data.y + data.z
+        messageLabel.text = String(format: "%.2f", sum)
+        
+        progressBar.progress = Float(abs(sum))
     }
     
     @IBAction func backButtonClicked(_ sender: UIButton) {
+        motionMgr.stopDeviceMotionUpdates()
         self.dismiss(animated: true, completion: nil)
     }
     
